56cfb9720f16e4eb73177febdaf17fb92b2025f9
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-02-05 20:26:54 +0200

    Finish the game on win or game over.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   window.fails = 0;
-  window.max_fails = 6;
+  window.max_fails = 5;
   window.possible_words = [
     "romania",
     "italy",
@@ -11,6 +11,8 @@ $(document).ready(function() {
   ];
   window.secret_word = "hangman";
   window.public_word = "_______";
+  window.game_is_finished = false;
+
 
   function choose_word() {
     var random_number = Math.floor(Math.random() * possible_words.length);
@@ -18,25 +20,33 @@ $(document).ready(function() {
     public_word = Array(secret_word.length).join("_");
   }
 
+
   function game_over() {
     $("p#status").text("Game over!");
+    game_is_finished = true;
   }
 
+
   function fail() {
-    fails++;
     $("img#main-image").attr("src", "./images/" + fails.toString() + ".png");
-    if(fails == max_fails) {
+    if(fails > max_fails) {
       game_over();
+    } else {
+      fails++;
     }
   }
 
+
   function win() {
     $("p#status").text("Congrats!");
+    game_is_finished = true;
   }
+
 
   function test_letter(letter) {
     return secret_word.indexOf(letter);
   }
+
 
   function replace_str(str, pos, value){
     var arr = str.split('');
@@ -44,31 +54,35 @@ $(document).ready(function() {
     return arr.join('');
   }
 
+
   function refresh_public_word() {
     $("p#public-word").text(public_word);
   }
+
 
   function start_game() {
     choose_word();
     refresh_public_word();
 
     $(document).keypress(function(event) {
-      var key = String.fromCharCode(event.which);
-      var test = test_letter(key);
-      if(test != -1) {
-        for(var i = 0; i <= secret_word.length; i++) {
-          if(secret_word[i] == key) {
-            public_word = replace_str(public_word, i, key);
+      if(!game_is_finished) {
+        var key = String.fromCharCode(event.which);
+        var test = test_letter(key);
+        if(test != -1) {
+          for(var i = 0; i <= secret_word.length; i++) {
+            if(secret_word[i] == key) {
+              public_word = replace_str(public_word, i, key);
+            }
           }
-        }
 
-        refresh_public_word();
+          refresh_public_word();
 
-        if(public_word == secret_word) {
-          win();
+          if(public_word == secret_word) {
+            win();
+          }
+        } else {
+          fail();
         }
-      } else {
-        fail();
       }
     });
   }
