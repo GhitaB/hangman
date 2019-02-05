f8214d16c57491ecbb09e2a41f1cba4301cea265
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-02-05 19:34:12 +0200

    Game over.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-  window.level = 0;
+  window.fails = 0;
+  window.max_fails = 6;
+
+  function game_over() {
+    $("p#status").text("Game over!");
+    $("button").hide();
+  }
+
+  function fail() {
+    fails++;
+    $("img#main-image").attr("src", "./images/" + fails.toString() + ".png");
+    if(fails == max_fails) {
+      game_over();
+    }
+  }
 
   $("button").on("click", function() {
-    level++;
-    $("img#main-image").attr("src", "./images/" + level.toString() + ".png");
+    fail();
   });
 });
