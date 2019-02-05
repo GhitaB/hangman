d06a5831e843e7ee3c4fe6792a1e7e24dfafcbed
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-02-05 19:27:00 +0200

    Levels.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  alert("AAA");
+  window.level = 0;
+
+  $("button").on("click", function() {
+    level++;
+    $("img#main-image").attr("src", "./images/" + level.toString() + ".png");
+  });
 });
